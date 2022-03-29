78f4bceafef4c37dc483f9ddc8b6c379a4cf156b
Nicolas <75789103+R3mazz@users.noreply.github.com>
2022-03-29 15:49:34 +0200

    added OsCheck.java

--- src/Gesser/Main.java
+++ src/Gesser/Main.java
@@ -3,11 +3,18 @@ package Gesser;
 import java.io.*;
 import java.awt.GraphicsEnvironment;
 public class Main{
+    static OsCheck.OSType ostype = OsCheck.getOperatingSystemType();
     public static void main (String [] args) throws IOException{
         Console console = System.console();
         if(console == null && !GraphicsEnvironment.isHeadless()){
             String filename = Main.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6);
-            Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""});
+
+            switch (ostype) {
+                case Windows: Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""}); break;
+                case MacOS: Runtime.getRuntime().exec("/bin/bash -c" + " java -jar \"" + filename); break;
+                case Linux: Runtime.getRuntime().exec("xterm" + " java -jar \"" + filename); break;
+                case Other: break;
+            }
         }else{
             WordGesser.main(new String[0]);
             System.out.println("Program has ended, please type 'exit' to close the console");
