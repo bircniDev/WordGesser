40d92946ff4a8fcd73b8de17c03a88f8dd037d15
R3mazz <75789103+R3mazz@users.noreply.github.com>
2022-03-30 10:56:31 +0200

    now works on macos

--- src/Gesser/Main.java
+++ src/Gesser/Main.java
@@ -11,28 +11,17 @@ public class Main{
             String filename = Main.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6);
 
             switch (ostype) {
-                case Windows: Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""}); break;
-                case MacOS:
-                    break;
-                case Linux: Runtime.getRuntime().exec("xterm" + " java -jar \"" + filename); break;
-                case Other: break;
+                case Windows ->
+                        Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start", "cmd", "/k", "java -jar \"" + filename + "\""});
+                case MacOS, Other -> {
+                    new WordGesser();
+                    WordGesser.main(null);
+                }
+                case Linux -> Runtime.getRuntime().exec("xterm" + " java -jar \"" + filename);
             }
         }else{
             WordGesser.main(new String[0]);
             System.out.println("Program has ended, please type 'exit' to close the console");
         }
-    }
-
-    public static void Macos(String filename) throws IOException {
-        //String[] args = new String[] { "java","-jar " + filename};
-        //Process proc = new ProcessBuilder(args).start();
-        //"/usr/bin/open -a Terminal", "java -jar " + filename + "\""
-        //String command = "java -jar "+filename;  "/bin/bash", "-c",
-        //Process proc = new ProcessBuilder("-a Terminal").start();
-        //Process proc = Runtime.getRuntime().exec(command);
-        //System.console();
-        Console console = System.console();
-        console.printf("java -jar ", filename);
-        console.flush();
     }
 }
